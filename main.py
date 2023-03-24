a059cefb3f51d19c7971363cabe7df7de41d7ec8
JH <44129179+lee-jian-hui@users.noreply.github.com>
2023-03-24 22:18:06 +0800

    initial commit to parse image data

--- main.py
+++ main.py
@@ -38,15 +38,119 @@ def parseGIF(bytez):
     # Image Descriptor & Each Frame
     # For amount of frames, parse image descriptor for frame and frame data
 
+    
+    
+# needs to add a code for each color in the color table
+def build_lzw_codetable(colortable):
+    # build the code table
+    pass
+    
+def gif_lzw_decoding():
+    # initialize code table
+    
+    
+    
+    # let CODE be the first code in the code stream
+    
+    
+    # output {CODE} to index stream
+    
+    
+    # set PREVCODE = CODE
+    
+    
+    # <LOOP POINT>
+    # let CODE be the next code in the code stream
+    # is CODE in the code table?
+    # Yes:
+    # output {CODE} to index stream
+    # let K be the first index in {CODE}
+    # add {PREVCODE}+K to the code table
+    # set PREVCODE = CODE
+    # No:
+    # let K be the first index of {PREVCODE}
+    # output {PREVCODE}+K to index stream
+    # add {PREVCODE}+K to code table
+    # set PREVCODE = CODE
+    # return to LOOP POINT
+        
+        
+        
+        
+    
+    
+    
+    
+def parseImageBlocks(size, bytes):
+    ret_arr = []
+    # this happens after parsiung the global color table and then the graphic control extension (which is optional)
+    
+        
+    
+    # first byte is the lzw minimum code size
+    lzw_min_codesize = bytes[0]
+    
+    # rest of the bytes are data sub-blocks
+    sub_blocks = bytes[1:]
+    
+    
+    # first byte of each data sub-block tells how bytes of actual data to follow
+    subblock_size = sub_blocks[0];
+    bytes_counter = 0
+    while subblock_size != 0: # while we havbent met block terminator
+        for i in range(subblock_size):
+            # refer to the color code table
+            # append something to the ret_arr
+            pass
+        
+        bytes_counter += subblock_size
+        subblock_size = sub_blocks[bytes_counter+1]# update the subblocm size
+    
+    return ret_arr
+
+
+
+# TODO: we skip the extensions for now instead of storing it 
+def skip_extensions(bytes):
+    
+    skip_counter = 0
+    
+    # skip plain text extension
+    
+    
+    # skip application extension
+    
+    
+    
+    # skip comment extension
+    
+    return skip_counter
+
 
 with open('test.gif', 'rb') as f:
     bytez = f.read()
 parseGIF(bytez)
 
+
 class GIF:
     def __init__(self, size):
         pass
+    
+    
+    
 
 class Frame:
     def __init__(self):
         pass
+    
+    def parseFrame(self, bytez):
+        pass
+    
+    
+    
+
+
+
+
+    
+
