880d7c44742c6d744ba393fd47c2c9b0c409d33a
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2023-05-08 13:26:06 +0800

    Show Separate Frames

--- gui.py
+++ gui.py
@@ -36,11 +36,25 @@ class DisplayTab(QWidget):
         self.parsed_img = img.parse()
 
     
+    def advance_frame(self, offset=1):
+        if self.cur_frame+offset < len(self.parsed_img.frames) and\
+            self.cur_frame+offset >= 0:
+            self.cur_frame += offset
+            self.update_canvas()
+            self.frame_label.setText(f"Cur Frame: {self.cur_frame}")
+    
     def initUI(self):
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.save_button = QPushButton()
-        self.save_button.setText("Save")
-        self.save_button.clicked.connect(lambda _: self.save_tab())
+
+        self.frame_label = QLabel("Cur Frame: 0")
+
+        self.next_frame_button = QPushButton()
+        self.next_frame_button.setText("Next Frame")
+        self.next_frame_button.clicked.connect(lambda _: self.advance_frame())
+
+        self.prev_frame_button = QPushButton()
+        self.prev_frame_button.setText("Prev Frame")
+        self.prev_frame_button.clicked.connect(lambda _: self.advance_frame(-1))
 
         self.label = QLabel()
         canvas = QPixmap(self.parsed_img.width, self.parsed_img.height)
@@ -49,21 +63,25 @@ class DisplayTab(QWidget):
         windowLayout = QVBoxLayout()
         hboxLayout = QHBoxLayout()
         hboxLayout.addWidget(self.label)
-        # windowLayout.addWidget(self.save_button)
+        hboxLayout.addWidget(self.next_frame_button)
+        hboxLayout.addWidget(self.prev_frame_button)
+        windowLayout.addWidget(self.frame_label)
 
         windowLayout.addLayout(hboxLayout)
         self.setLayout(windowLayout)
         
         self.show()
     def update_canvas(self):
-        canvas = self.label.pixmap()
+        # canvas = self.label.pixmap()
+        canvas = QPixmap(self.parsed_img.width, self.parsed_img.height)
+        canvas.fill(Qt.black)
         painter = QtGui.QPainter(canvas)
         pen = QtGui.QPen()
         pen.setWidth(1)
         for y in range(self.parsed_img.height):
             for x in range(self.parsed_img.width):
-                first_frame = self.parsed_img.frames[0]
-                rgb = first_frame.frame_img_data.rgb_lst[y * self.parsed_img.width + x]
+                drawn_frame = self.parsed_img.frames[self.cur_frame]
+                rgb = drawn_frame.frame_img_data.rgb_lst[y * self.parsed_img.width + x]
                 pen.setColor(QtGui.QColor(rgb.r, rgb.g, rgb.b))
                 # pen.setColor(QtGui.QColor('red'))
                 painter.setPen(pen)
@@ -76,6 +94,7 @@ class DisplayTab(QWidget):
 if __name__ == '__main__':
     def add_tabs(tabs):
         tabs.clear()
+        w.resize(800, 500)
         my_file  = QFileDialog.getOpenFileName(None, "Select GIF file", "", "Images (*.gif)")
         my_file = my_file[0]
         tabs.resize(800, 1000)
@@ -88,22 +107,18 @@ if __name__ == '__main__':
     apply_stylesheet(app, theme='light_blue.xml', invert_secondary=True)
 
     w = QWidget()
-    w.resize(800, 500)
+    w.resize(800, 100)
     vbox = QVBoxLayout()
 
-    open_folder = QPushButton()
-    open_folder.setText("Open GIF file")
+    open_file = QPushButton()
+    open_file.setText("Open GIF file")
 
-    pull_savegames= QPushButton()
-    pull_savegames.setText("Pull Savegames")
-
-    push_savegames= QPushButton()
-    push_savegames.setText("Push Savegames")
     tabs = QTabWidget()
-    open_folder.clicked.connect(lambda: add_tabs(tabs))
+    tabs.resize(0, 0)
+    open_file.clicked.connect(lambda: add_tabs(tabs))
 
     vbox.addWidget(tabs)
-    vbox.addWidget(open_folder)
+    vbox.addWidget(open_file)
     w.setLayout(vbox)
     w.show()
     # ex = App()
