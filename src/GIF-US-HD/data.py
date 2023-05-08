78e2b94dc6d641b5579c30d8a957c81afaadcb41
LemonTea387 <JunJieLow08@gmail.com>
2023-05-09 00:45:44 +0800

    Removed ImageData as not required.

--- src/GIF-US-HD/data.py
+++ src/GIF-US-HD/data.py
@@ -11,8 +11,8 @@ TODO: make changes to GifFrame such that it has:
 class GifFrame:
     def __init__(self):
         self.extensions = []
-        self.img_descriptor = None    #  NOTE: the img descriptor contians the LCT (local color table) as well as the width and height of the frame
-        self.frame_img_data = None # NOTE: in the form of a 1D list of RGB triplets
+        self.img_descriptor = None
+        self.frame_img_data = None # NOTE: in the form of a 1D list of Indices to CT
 
 class GifData:
     def __init__(self):
@@ -31,37 +31,8 @@ class GifData:
         self.frames:list[GifFrame] = []
 
 
-# TODO: rework the ImageData class for each frame instead of handling for the whole GifData class
-class ImageData:
-    def __init__(self, bytez):
-        # TODO: now we still assume that each image frame matches the canva size of the GIF
-        self.bytez = bytez     
-        self.rgb_lst = None
-    
-    def __repr__(self):
-        string = f"ImageData[{self.width}x{self.height})]\n"
-        for i in range(len(self.frames_rgb)):
-            string += f"frame{i}:[{self.frames_rgb[i]}]\n" 
-        return string
-    
-    # TODO: returns list of raw bytes as 2D list of imagedata
-    def bytez_2_frames_rgb(bytez_lst, width, height):
-        frames_rgb = []
-        # calculate the size of the image in bytes
-        image_size = width * height * 3
-        # loop through each frames' raw bytes
-        for i in range(len(bytez_lst)):
-            bytez = bytez_lst[i]
-            # unpack the raw bytes into RGB triplets
-            frame_rgb_data = []
-            for j in range(0, image_size, 3):
-                r, g, b = struct.unpack("BBB", bytez[j:j+3])
-                frame_rgb_data.append(RGB(r, g, b))
-            # append rgb triplets in this frame to 
-            frames_rgb.append(frame_rgb_data)
-        return frames_rgb
-
 class ImageDescriptor:
+    IMAGE_SEPARATOR = 0x2C
     def __init__(self):
         self.left = None
         self.top = None
@@ -78,10 +49,9 @@ class ImageDescriptor:
     
     @staticmethod
     def is_image_descriptor(bytez, offset):
-        IMAGE_SEPARATOR = 0x2C
         if offset >= len(bytez):
             raise Exception("Invalid offset access for image descriptor!!")
-        return bytez[offset] == IMAGE_SEPARATOR
+        return bytez[offset] == ImageDescriptor.IMAGE_SEPARATOR
     
 class RGB:
     def __init__(self, r, g, b):
