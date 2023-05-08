df3b2460290ac18b8d98522fa865891297fab2b5
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2023-05-08 22:14:36 +0800

    Fixed bug in LZW compression

--- lzw_gif.py
+++ lzw_gif.py
@@ -21,7 +21,7 @@ def compress(index_stream, lzw_min_code_size):
     code_stream += lzw_min_code_size.to_bytes(byteorder="big", length=1)
     code_table = create_code_table(lzw_min_code_size)
 
-    lst_to_str = lambda lst: ",".join([str(_) for _ in lst])
+    lst_to_str = lambda lst: ",".join([str(_[0]) for _ in lst])
     str_to_lst = lambda s: [int(_) for _ in s.split(",")]
 
     def gen_code_stream(index_stream, lzw_min_code_size, code_table):
@@ -33,28 +33,50 @@ def compress(index_stream, lzw_min_code_size):
         code_stream.append((code_table[CLEAR_CODE], cur_code_size))
 
         first_val = index_stream[0]
-        index_buffer = [first_val]
+        index_buffer = [(first_val, cur_code_size)]
 
         next_smallest_code = (2 ** lzw_min_code_size)+2
 
         for next_ptr in range(1, len(index_stream)):
-            k = [index_stream[next_ptr]]
+            k = [(index_stream[next_ptr], cur_code_size)]
 
             if lst_to_str(index_buffer+k) in code_table:
                 index_buffer = index_buffer + k
             else:
+                if next_smallest_code == 4096:
+                    # Reset
+                    code_table = create_code_table(lzw_min_code_size)
+                    code_stream.append((code_table[CLEAR_CODE], cur_code_size))
+                    index_buffer = k
+                    next_smallest_code = (2 ** lzw_min_code_size)+2
+
                 code_table[lst_to_str(index_buffer + k)] = next_smallest_code
                 code_stream.append((code_table[lst_to_str(index_buffer)], cur_code_size))
                 if next_smallest_code == 2 ** cur_code_size:
                     cur_code_size += 1
                 next_smallest_code += 1
                 index_buffer = k
-        code_stream += index_buffer
+        code_stream.append((code_table[lst_to_str(index_buffer)], cur_code_size))
         # Send EOI
         code_stream.append((code_table[EOI_CODE], cur_code_size))
         return code_stream
-    print(gen_code_stream(index_stream, lzw_min_code_size, code_table))
+    code_stream = gen_code_stream(index_stream, lzw_min_code_size, code_table)
 
+    def append_bits(a, b):
+        num_a, num_a_len = a
+        num_b, num_b_len = b
+        return ((num_a << num_b_len) | num_b, num_a_len + num_b_len)
+
+    bitstream = (0, 0)
+    for code in code_stream:
+        bitstream = append_bits(code, bitstream)
+
+    # Padding
+    pad_num = 8 - (bitstream[1] % 8)
+    bitstream = (bitstream[0], bitstream[1] + pad_num)
+
+    ret = bitstream[0].to_bytes(byteorder="little", length=bitstream[1] // 8)
+    return ret
 
 
 
@@ -67,5 +89,5 @@ decompressed_data = decompress(compressed_data)
 lzw_min_code_size = compressed_data[0]
 decompressed_data = [1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,2,2,2,1,1,1,0,0,0,0,2,2,2,1,1,1,0,0,0,0,2,2,2,2,2,2,0,0,0,0,1,1,1,2,2,2,0,0,0,0,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1]
 compressed_data = compress(decompressed_data, lzw_min_code_size)
-
+print(compressed_data)
 
